3488409dc41ae227e5890914a6a294bcf9547598
ruslanskira <skira.ruslan@gmail.com>
2018-01-01 21:44:55 +0200

    I solve the task with unit testing

--- src/calc.js
+++ src/calc.js
@@ -7,40 +7,41 @@ function Calculator (arg1, arg2, operator) {
     this._operator = operator;
 }
 
-Calculator.prototype.calculate = function () {
+Calculator.prototype._calculate = function () {
     if (this._operator === '+') {
-        return this._arg1 + this._arg2;
+        return( this._arg1 + this._arg2 );
     }
     else if (this._operator == '-' ) {
-        return this._arg1 - this._arg2;
+        return(this._arg1 - this._arg2);
     }
     else if ( this._operator == '*') {
-        return this._arg1 * this._arg2;
+        return(this._arg1 * this._arg2);
     }
     else if (this._operator == '/') {
-        return this._arg1 /this._arg2;
+        return(this._arg1 /this._arg2);
     }
     else {
         console.log('DIch?')
     };
 };
-var myCalc1 = new Calculator(3, 4, '+');
-myCalc1.calculate();
+var myCalc1 = new Calculator(4, 9, '+');
+myCalc1._calculate();
+
 
 //Unit test
-function TestCalc (output, answer) {
+function TestCalc ( output, answer ) {
     this._output = output;
     this._answer = answer;
 };
 TestCalc.prototype.check = function () {
-    if (this._output == this._answer) {
+    if (this._output === this._answer) {
         console.log('OK');
     } else {
         console.log('dich prodolgaetsa?');
     };
 };
 
-var checkMyCalc1 = new TestCalc(9, 9);
+var checkMyCalc1 = new TestCalc(myCalc1._calculate(), 13 );
 checkMyCalc1.check();
 //myCalc1.calculate(4, 5, '+')
 
