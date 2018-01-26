a6e6451c27097ef8f5a74570205dd2abc40678e9
ruslanskira <skira.ruslan@gmail.com>
2018-01-26 22:52:03 +0200

    trying to work in one file

--- src/calc.js
+++ src/calc.js
@@ -1,31 +1,30 @@
-function Calculator () {}
+class Calculator {
+    constructor(){}
 
-Calculator.prototype.calculate = function (arg1, arg2, operator) {
-    if (operator === '+') {
-        return( arg1 + arg2 );
+
+    calculate(arg1, arg2, operator) {
+        if (operator === '+') {
+            return( arg1 + arg2 );
+        }
+        else if (operator == '-' ) {
+            return(arg1 - arg2);
+        }
+        else if ( operator == '*') {
+            return(arg1 * arg2);
+        }
+        else if (operator == '/') {
+            return(arg1 / arg2);
+        }
+        else {
+            console.log('DIch?');
+        };
     }
-    else if (operator == '-' ) {
-        return(arg1 - arg2);
-    }
-    else if ( operator == '*') {
-        return(arg1 * arg2);
-    }
-    else if (operator == '/') {
-        return(arg1 / arg2);
-    }
-    else {
-        console.log('DIch?');
-    };
-};
-var myCalc = new Calculator();
-const answer = myCalc.calculate(6, 2, '+');
-console.log(answer);
+}
+
+const counting = new Calculator();
 
 
-const answer2 = myCalc.calculate(3, 2,'/');
-console.log(answer2);
-
-
+//console.log(counting.calculate(10,5,'/'));
 //Unit test
 
 function TestCalc () {};
@@ -37,20 +36,8 @@ TestCalc.prototype.equals = function (output, answer, wrongAnsw) {
         console.log(wrongAnsw);
     };
 };
-const calcForTest = new Calculator();
+const calcForTest = new Calculator()
 const test = new TestCalc();
 const testCalc1 = test.equals(calcForTest.calculate(5, 3, '-'), 2, 'hoho');
 
 console.log(testCalc1); //why it is undefined
-
-
-
-//checkMyCalc1.check(myCalc1.calculate(), 13, 'Calculate wrong addition' );
-//myCalc1.calculate(4, 5, '+')
-
-
-
-
-
-
-
