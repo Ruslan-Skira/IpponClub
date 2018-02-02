759bf8cf73db02e93a8889a2a53d485c285c6e5a
spiridonov-oa <spiridonov.oa@gmail.com>
2018-02-03 01:55:21 +0530

    Add comments

--- src/calc.js
+++ src/calc.js
@@ -1,8 +1,6 @@
 class Calculator {
     constructor(){};
 
-
-
     calculate(arg1, arg2, operator) {
         if (operator === '+') {
             return( arg1 + arg2 );
@@ -24,26 +22,25 @@ class Calculator {
 
 const counting = new Calculator();
 
-
 console.log(counting.calculate(10,5,'/'));
 
 var indexCalculator={};
 var str = prompt('fill up 1+2');
 
-var operators = " '+', '-', '/', '*' ";
-for(var i = 0; i<operators.length; i++){
+var operators = " '+', '-', '/', '*' "; //create array as class property on init this.operators: ['+','-','/','*']
+for(var i = 0; i<operators.length; i++){ // make this as method of class Calculator to be able to use it inside
     var ind = str.indexOf(operators[i]);
 
-    if(str[ind]){
+    if(str[ind]){   //check if this.operators.indexOf(str[i]);
         indexCalculator.arg1 = str[ind-1];
         indexCalculator.arg2 = str[ind+1];
         indexCalculator.operator = str[ind];
     }
-    else {
+    else { // else - this is not operator. So add it to the numbers array. this.numbers...
         console.log('in progress');
     }
 }
-    alert(indexCalculator.operator);
+alert(indexCalculator.operator);
 
 
 
