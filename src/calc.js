8c85476cab6700153b66906e2b224a6cd3962686
ruslanskira <skira.ruslan@gmail.com>
2018-02-09 09:32:54 +0200

    Write with array

--- src/calc.js
+++ src/calc.js
@@ -1,21 +1,19 @@
 class Calculator {
     constructor(){
         this.operators = ['+', '-', '/', '*']; //create array as class property on init this.operators: ['+','-','/','*']
-        this.numbers = //hz;
+        this.numbers = [];//hz;
     };
 
 
-    calculate(arg1, arg2, operator) {
-        for(var i = 0; i < operators.length; i++){ // make this as method of class Calculator to be able to use it inside
-            var ind = str.indexOf(operators[i]);
+    calculate() {
+        for(var i = 0; i < this.operators.length; i++){ // make this as method of class Calculator to be able to use it inside
+            var ind = str.indexOf(this.operators[i]);
 
             if(str[ind]){   //check if this.operators.indexOf(str[i]);
-                indexCalculator.arg1 = str[ind-1];
-                indexCalculator.arg2 = str[ind+1];
-                indexCalculator.operator = str[ind];
+              console.log('I am done');
             }
             else { // else - this is not operator. So add it to the numbers array. this.numbers...
-                console.log('in progress');
+                this.numbers.push(str[i]);
             }
         }
     }
@@ -23,28 +21,14 @@ class Calculator {
 
 const counting = new Calculator();
 
-console.log(counting.calculate(10,5,'/'));
-
-var indexCalculator={};
 var str = prompt('fill up 1+2');
-
-
-
-
-alert(indexCalculator.operator);
-
-
-
-
-
-
-
+console.log(counting.calculate());
 
 
 
 //Unit test
 
-function TestCalc () {};
+/*function TestCalc () {};
 
 TestCalc.prototype.equals = function (output, answer, wrongAnsw) {
     if (output === answer) {
@@ -57,4 +41,4 @@ const calcForTest = new Calculator();
 const test = new TestCalc();
 const testCalc1 = test.equals(calcForTest.calculate(5, 3, '-'), 2, 'hoho');
 
-console.log(testCalc1); //why it is undefined
+console.log(testCalc1); //why it is undefined*/
