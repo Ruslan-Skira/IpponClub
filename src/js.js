0365ac1ade4d8eec97bb8fbd6a1fae48a4b84f72
ruslanskira <skira.ruslan@gmail.com>
2018-04-02 19:35:39 +0300

    changed pop on shift

--- src/js.js
+++ src/js.js
@@ -39,13 +39,13 @@ class Calculator{
         return this.operatorsList.indexOf(char) !==-1;
     };
     calculateAvailable(){
-        if (this.parsed.operators && this.parsed.operands > 1) {
-            const result = this.runCalculation(+this.parsed.operands.pop(), +this.parsed.operands.pop(), this.parsed.operators.pop());
+        if (this.parsed.operators.length && this.parsed.operands.length > 1) {
+            const result = this.runCalculation(+this.parsed.operands.shift(), +this.parsed.operands.shift(), this.parsed.operators.shift());
             this.parsed.operands.push(result);
         }
 
     }
-    runCalculation(arg2, arg1, operator){
+    runCalculation(arg1, arg2, operator){
         if (operator === this.operatorsList[0]){
            return arg1 + arg2;
         } else if (operator === this.operatorsList[1]){
@@ -61,9 +61,20 @@ class Calculator{
 }
 
 
-
-
-
-
 const calculator = new Calculator();
 console.log(calculator.calculate('1 + 2'));
+
+//*** Unit test ***
+
+function TestCalculator () {};
+TestCalculator.prototype.equals = function (result, expected, description){
+    if(result === expected){
+        console.log('passed -' + description);
+    } else {
+        console.log('FAILED-' + 'resuls=' + result + 'expected' + expected + description);
+    }
+}
+const CalcForTest = new Calculator();
+const test = new TestCalculator();
+
+test.equals(CalcForTest.calculate('2 * 2 + 2'), 6, 'counting right');
